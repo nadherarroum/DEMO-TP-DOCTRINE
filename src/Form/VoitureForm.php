a06b6591fa622075b16be6ffdca792595c02025a
Nadher <nadher.arroum@gmail.com>
2021-10-21 16:21:00 +0100

    ADD type for inputs

--- src/Form/VoitureForm.php
+++ src/Form/VoitureForm.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Voiture;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,10 @@ class VoitureForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Serie')
-            ->add('dateMiseEnMarche')
-            ->add('modele')
-            ->add('prixJour')
+            ->add('Serie', TextType::class)
+            ->add('dateMiseEnMarche', DateType::class)
+            ->add('modele', TextType::class)
+            ->add('prixJour', TextType::class)
         ;
     }
 
